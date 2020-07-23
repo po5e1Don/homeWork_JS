2dcbb827eb647c09f2c244007bd1c4b1d01c5142
po5e1Don <nickolaydaragan@gmail.com>
2020-07-23 11:53:26 +0300

    comment

--- hw1/js/index.js
+++ hw1/js/index.js
@@ -1,7 +1,9 @@
 //Task#1
+
 console.log("My first debug");
 
 //Task#2
+
 var age = 8;
 console.log(age);
 /*Я для себя решил что в этом таске имя питомца неизменное
@@ -10,9 +12,12 @@ const name = "Mary";
 console.log(name);
 
 //Task#3
+
 var age = 9;
 console.log(age);
+
 //Task#4
+
 let num = 1;
 console.log(typeof num);
 
@@ -23,21 +28,25 @@ let trueOrFalse = true;
 console.log(typeof trueOrFalse);
 
 //Task5
-const student = {
-  name: "{Ваше имя}",
-  surname: "{Ваша фамилия}",
-  isActive: true,
-};
-student[typeof isActive];
+
+// const student = {
+//   name: "{Ваше имя}",
+//   surname: "{Ваша фамилия}",
+//   isActive: true,
+// };
+
 //Task#6
+
 let fruits = ["orange ", "lime", "strawberry", "mango", "peach"];
 console.log(fruits);
 let numbers = [0, 1, 2, 3, 4];
 console.log(numbers);
 let students = ["student1", "student2", "student3", "student4", "student5"];
 console.log(students);
+
 //HARD LEVEL
 //Task#1
+
 var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var first = array[0];
 var last = array[array.length - 1];
@@ -45,8 +54,10 @@ console.log(first);
 console.log(last);
 
 //Task#2
-const student = {
-  name: "{Ваше имя}",
-  surname: "{Ваша фамилия}",
-  isActive: true,
-};
+
+// var student = {
+//   name: "{Ваше имя}",
+//   surname: "{Ваша фамилия}",
+//   isActive: true,
+// };
+// student[(name, surname, isActive)];
