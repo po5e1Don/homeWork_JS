a589f591d3015854c02e0dfb7e7c0ff783f2acaa
po5e1Don <nickolaydaragan@gmail.com>
2020-07-23 11:35:32 +0300

    Task1-6

---
+++ hw1/js/index.js
@@ -0,0 +1,52 @@
+//Task#1
+console.log("My first debug");
+
+//Task#2
+var age = 8;
+console.log(age);
+/*Я для себя решил что в этом таске имя питомца неизменное
+ поэтому использую константу*/
+const name = "Mary";
+console.log(name);
+
+//Task#3
+var age = 9;
+console.log(age);
+//Task#4
+let num = 1;
+console.log(typeof num);
+
+let str = "one";
+console.log(typeof str);
+
+let trueOrFalse = true;
+console.log(typeof trueOrFalse);
+
+//Task5
+const student = {
+  name: "{Ваше имя}",
+  surname: "{Ваша фамилия}",
+  isActive: true,
+};
+student[typeof isActive];
+//Task#6
+let fruits = ["orange ", "lime", "strawberry", "mango", "peach"];
+console.log(fruits);
+let numbers = [0, 1, 2, 3, 4];
+console.log(numbers);
+let students = ["student1", "student2", "student3", "student4", "student5"];
+console.log(students);
+//HARD LEVEL
+//Task#1
+var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var first = array[0];
+var last = array[array.length - 1];
+console.log(first);
+console.log(last);
+
+//Task#2
+const student = {
+  name: "{Ваше имя}",
+  surname: "{Ваша фамилия}",
+  isActive: true,
+};
